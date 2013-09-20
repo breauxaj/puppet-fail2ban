8ea491ff310f48276627b6cf797c34db69a4617d
Adam Breaux <breauxaj@gmail.com>
2013-09-19 20:21:02 -0400

    puppet lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,21 +1,8 @@
 class fail2ban {
-  $required = $operatingsystem ? {
+  $required = $::operatingsystem ? {
     /(?i-mx:centos|fedora|redhat|scientific)/ => 'fail2ban',
   }
 
   package { $required: ensure => latest }
-
-}
-
-define fail2ban::service ( $ensure = running,
-                           $enable = true ) {
-  $service = $operatingsystem ? {
-    /(?i-mx:centos|fedora|redhat|scientific)/ => 'fail2ban',
-  }
-
-  service { $service:
-    ensure => $ensure,
-    enable => $emable,
-  }
 
 }
---
+++ manifests/service.pp
@@ -0,0 +1,14 @@
+define fail2ban::service (
+  $ensure = running,
+  $enable = true
+) {
+  $service = $::operatingsystem ? {
+    /(?i-mx:centos|fedora|redhat|scientific)/ => 'fail2ban',
+  }
+
+  service { $service:
+    ensure => $ensure,
+    enable => $enable,
+  }
+
+}
