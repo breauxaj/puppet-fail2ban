bd345c90ade2e6c85643879a736c308e1ea1b6f0
Adam Breaux <breauxaj@gmail.com>
2014-07-21 14:09:18 -0400

    added docs

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,31 @@
+# Class: fail2ban
+#
+# This class installs the fail2ban server
+#
+# Parameters:
+#
+#  ensure: (default latest)
+#    Determine the state of the packages
+#
+# Actions:
+#   - Installs the fail2ban package
+#
+# Requires:
+#
+# Sample Usage:
+#
+#  For a standard installation, use:
+#
+#    class { 'fail2ban':
+#      ensure => 'latest'
+#    }
+#
+#  To remove the installation, use:
+#
+#    class { 'fail2ban':
+#      ensure => 'absent'
+#    }
+#
 class fail2ban (
   $ensure = 'latest'
 ){
--- manifests/service.pp
+++ manifests/service.pp
@@ -1,3 +1,29 @@
+# Define: fail2ban::service
+#
+# This define manages the Fail2ban service
+#
+# Parameters:
+#
+#  ensure:
+#    Controls the state of the service
+#
+#  enable:
+#    Controls the service start on boot
+#
+# Actions:
+#   - Stops/starts the Fail2ban service
+#
+# Requires:
+#
+# Sample Usage:
+#
+#  To enable the service, use:
+#
+#    fail2ban::service { 'default':
+#      ensure => running,
+#      enable => true
+#    }
+#
 define fail2ban::service (
   $ensure = running,
   $enable = true
